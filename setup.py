56cb06ac70db62518acd3733a35ea17deffdc4de
Jack <jack.grahl@yahoo.co.uk>
2015-10-13 11:07:24 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='qual',
+    version='0.1',
+    description='Calendar stuff',
+    url='http://github.com/jwg4/qual',
+    author='Jack Grahl',
+    author_email='jack.grahl@yahoo.co.uk',
+    license='Apache License 2.0',
+    packages=['qual'],
+)
+
