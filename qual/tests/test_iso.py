489129bd26f72967fbbc279ab488fe047714a250
Jack <jack.grahl@yahoo.co.uk>
2015-10-13 12:59:54 +0100

    Use timezones=[] to underline that we only care about dates.

--- qual/tests/test_iso.py
+++ qual/tests/test_iso.py
@@ -7,7 +7,7 @@ import qual
 from datetime import date
 
 class TestIsoUtils(unittest.TestCase):
-    @given(datetimes())
+    @given(datetimes(timezones=[]))
     def test_round_trip_date(self, dt):
         d = dt.date()
         self.assertEqual(qual.iso_to_gregorian(*d.isocalendar()), d)
