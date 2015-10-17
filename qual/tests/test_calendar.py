6f45e82af789586baf7354b562bbb1587d94b28c
Jack <jack.grahl@yahoo.co.uk>
2015-10-17 22:29:13 +0100

    Add a test for a date missing from English historical calendars.

--- qual/tests/test_calendar.py
+++ qual/tests/test_calendar.py
@@ -19,6 +19,13 @@ class TestProlepticGregorianCalendar(unittest.TestCase):
         """Pope Gregory introduced the calendar in 1582"""
         self.check_valid_date(1200, 2, 29)
 
+    def test_day_missed_out_in_British_calendar_change(self):
+        """This date never happened in English law:
+           It was missed when changing from the Julian to
+           Gregorian. This test proves that we are not
+           using a historical British calendar."""
+        self.check_valid_date(1752, 9, 3)
+
     def test_Julian_leap_day_is_not_a_valid_date(self):
         """This day /was/ a leap day contemporaneously,
            but is not a valid date of the Gregorian calendar."""
