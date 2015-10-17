90974a088813dcc3a0c4a7cae5758f67c4b52a15
Jack <jack.grahl@yahoo.co.uk>
2015-10-17 22:09:51 +0100

    Check a leap year date from before the start of the calendar.
    This is not really a strong test of the proleptic calendar. All days back to year 1 which are valid in the Julian calendar are valid in the Gregorian calendar.

--- qual/tests/test_calendar.py
+++ qual/tests/test_calendar.py
@@ -8,7 +8,10 @@ class TestProlepticGregorianCalendar(unittest.TestCase):
     def setUp(self):
         self.calendar = qual.ProlepticGregorianCalendar()
 
-    def test_valid_date(self):
-        d = self.calendar.date(1200, 2, 29)
+    def check_valid_date(self, year, month, day):
+        d = self.calendar.date(year, month, day)
         self.assertIsNotNone(d)
 
+    def test_leap_year_from_before_1582(self):
+        """Pope Gregory introduced the calendar in 1582"""
+        self.check_valid_date(1200, 2, 29)
