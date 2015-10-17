1c2b6c0daea1d04985ef6ddff35527ba207ec191
Jack <jack.grahl@yahoo.co.uk>
2015-10-17 22:14:32 +0100

    Check that a certain date is invalid.
    This distinguishes correctly between the proleptic Gregorian calendar, and the historical or astronomical calendars, where this date would be valid.

--- qual/tests/test_calendar.py
+++ qual/tests/test_calendar.py
@@ -12,6 +12,15 @@ class TestProlepticGregorianCalendar(unittest.TestCase):
         d = self.calendar.date(year, month, day)
         self.assertIsNotNone(d)
 
+    def check_invalid_date(self, year, month, day):
+        self.assertRaises(Exception, lambda : self.calendar(year, month, day))
+
     def test_leap_year_from_before_1582(self):
         """Pope Gregory introduced the calendar in 1582"""
         self.check_valid_date(1200, 2, 29)
+
+    def test_Julian_leap_day_is_not_a_valid_date(self):
+        """This day /was/ a leap day contemporaneously,
+           but is not a valid date of the Gregorian calendar."""
+        self.check_invalid_date(1300, 2, 29)
+
