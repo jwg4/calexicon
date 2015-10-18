002d1ac1d2fcf88a7df46681ef7b3969f08e9a8f
Jack <jack.grahl@yahoo.co.uk>
2015-10-18 08:57:16 +0100

    Allow conversion from Julian to ProlepticGregorian, also comparison of dates.

--- qual/calendar.py
+++ qual/calendar.py
@@ -1,17 +1,27 @@
-from datetime import date
+from datetime import date, timedelta
 
 class DateWithCalendar(object):
     def __init__(self, calendar_class, date):
         self.calendar = calendar_class
         self.date = date
 
+    def convert_to(self, calendar):
+        return calendar.from_date(self.date)
+
+    def __eq__(self, other):
+        return self.calendar == other.calendar and self.date == other.date
+
 class ProlepticGregorianCalendar(object):
     def date(self, year, month, day):
         d = date(year, month, day)
         return DateWithCalendar(ProlepticGregorianCalendar, d)
 
+    def from_date(self, date):
+        return DateWithCalendar(ProlepticGregorianCalendar, date)
+
 class JulianCalendar(object):
     def date(self, year, month, day):
         d = date(year, month, day)
+        d = d + timedelta(days=10)
         return DateWithCalendar(JulianCalendar, d)
 
