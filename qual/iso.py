41c7d60556dff4be1c5f39cf694470d3af4869f0
Jack <jack.grahl@yahoo.co.uk>
2015-11-01 00:08:44 +0000

    Add checks for a reasonable week number.

--- qual/iso.py
+++ qual/iso.py
@@ -1,7 +1,12 @@
 from datetime import date, timedelta
 
 def iso_to_gregorian(year, week, weekday):
+    if week < 1 or week > 54:
+        raise ValueError("Week number %d is invalid for an ISO calendar." % (week, ))
     jan_8 = date(year, 1, 8).isocalendar()
     offset = (week - jan_8[1]) * 7 + (weekday - jan_8[2])
-    return date(year, 1, 8) + timedelta(days=offset)
+    d = date(year, 1, 8) + timedelta(days=offset)
+    if d.isocalendar()[0] != year:
+        raise ValueError("Week number %d is invalid for ISO year %d." % (week, year))
+    return d
 
