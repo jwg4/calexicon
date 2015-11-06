3336ed47d18c9954189a6384f2965a80b91a3019
Jack <jack.grahl@yahoo.co.uk>
2015-11-06 21:45:51 +0000

    Changes to make DateWithCalendar inherit from date, didnt seem to work.

--- qual/calendars/dates.py
+++ qual/calendars/dates.py
@@ -3,54 +3,21 @@ from datetime import date, timedelta
 class InvalidDate(Exception):
     pass
 
-class DateWithCalendar(object):
+class DateWithCalendar(date):
     def __init__(self, calendar_class, d):
         self.calendar = calendar_class
-        self._date = d
+        date.__init__(self, d.year, d.month. d.day)
 
     def convert_to(self, calendar):
-        return calendar.from_date(self._date)
+        return calendar.from_date(self)
 
     def __eq__(self, other):
-        return self.calendar == other.calendar and self._date == other._date
+        if super(DateWithCalendar, self) != super(DateWithCalendar, other):
+            return False
+        return self.calendar == other.calendar
 
     def __str__(self):
-        return "%s (%s)" % (self.calendar.date_display_string(self._date), self.calendar.display_name)
-
-    def __lt__(self, other):
-        try:
-            other_date = other._date
-        except:
-            other_date = other
-        return self._date < other_date
-
-    def __gt__(self, other):
-        try:
-            other_date = other._date
-        except:
-            other_date = other
-        return self._date > other_date
-
-    def __le__(self, other):
-        try:
-            other_date = other._date
-        except:
-            other_date = other
-        return self._date <= other_date
-
-    def __ge__(self, other):
-        try:
-            other_date = other._date
-        except:
-            other_date = other
-        return self._date >= other_date
-
-    def __sub__(self, other):
-        try:
-            other_date = other._date
-        except:
-            other_date = other
-        return self._date - other_date
+        return "%s (%s)" % (self.calendar.date_display_string(self), self.calendar.display_name)
 
     @staticmethod
     def make_assertEqual(test_case):
