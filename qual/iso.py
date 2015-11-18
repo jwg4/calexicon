959464b5100393ef46ed8024e6c781a8667270d2
Jack <jack.grahl@yahoo.co.uk>
2015-11-18 00:26:32 +0000

    Add an OverflowDate class to represent a date beyond the normal range.
    In this case we only hope to convert the date back into an ISO date by remembering where we got it from.

--- qual/iso.py
+++ qual/iso.py
@@ -1,11 +1,16 @@
 from datetime import date, timedelta
 
+from overflow import OverflowDate
+
 def iso_to_gregorian(year, week, weekday):
     if week < 1 or week > 54:
         raise ValueError("Week number %d is invalid for an ISO calendar." % (week, ))
     jan_8 = date(year, 1, 8).isocalendar()
     offset = (week - jan_8[1]) * 7 + (weekday - jan_8[2])
-    d = date(year, 1, 8) + timedelta(days=offset)
+    try:
+        d = date(year, 1, 8) + timedelta(days=offset)
+    except:
+        d = OverflowDate(isocalendar=(year, week, weekday))
     if d.isocalendar()[0] != year:
         raise ValueError("Week number %d is invalid for ISO year %d." % (week, year))
     return d
