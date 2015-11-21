6dd322636ed922d40d2d1ae6a66a67f9e9c2b7d0
Jack <jack.grahl@yahoo.co.uk>
2015-11-21 22:00:09 +0000

    Use the custom assertEqual for DateWithCalendar in these test cases.

--- calexicon/calendars/tests/test_calendar.py
+++ calexicon/calendars/tests/test_calendar.py
@@ -47,6 +47,10 @@ class TestProlepticGregorianCalendar(CalendarTest):
 class TestJulianCalendar(CalendarTest):
     def setUp(self):
         self.calendar = JulianCalendar()
+        self.addTypeEqualityFunc(
+            DateWithCalendar,
+            DateWithCalendar.make_assertEqual(self)
+        )
 
     def test_display_string(self):
         """ The display string for a date gives the correct date and specifies the calendar. """
@@ -72,6 +76,10 @@ class TestJulianCalendar(CalendarTest):
 class TestProlepticJulianCalendar(TestJulianCalendar):
     def setUp(self):
         self.calendar = ProlepticJulianCalendar()
+        self.addTypeEqualityFunc(
+            DateWithCalendar,
+            DateWithCalendar.make_assertEqual(self)
+        )
 
     def test_year_1_bce(self):
         self.check_valid_date(-1, 3, 1)
