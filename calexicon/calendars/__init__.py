26171a67969ebf8af963403be99ba8a7eb46c4dc
Jack <jack.grahl@yahoo.co.uk>
2015-11-22 22:22:49 +0000

    Add a pointer to the list of calendars in README.

--- calexicon/calendars/__init__.py
+++ calexicon/calendars/__init__.py
@@ -1,3 +1,16 @@
+__all__ = [
+    # Dates
+    'DateWithCalendar',
+    'InvalidDate',
+    # Calendars
+    'JulianCalendar',
+    'ProlepticGregorianCalendar',
+    'ProlepticJulianCalendar',
+    'EnglishHistoricalCalendar',
+    'SpanishHistoricalCalendar',
+    'FrenchHistoricalCalendar'
+]
+
 from dates import DateWithCalendar, InvalidDate
 from main import JulianCalendar, ProlepticGregorianCalendar, ProlepticJulianCalendar
 from historical import EnglishHistoricalCalendar, SpanishHistoricalCalendar, FrenchHistoricalCalendar
