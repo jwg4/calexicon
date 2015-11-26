d6ba0ab2d3ecb112e7f9b306b1e96e99feff676a
Jack <jack.grahl@yahoo.co.uk>
2015-11-26 23:50:49 +0000

    Use __all__ in modules import by the main package.

--- calexicon/dates.py
+++ calexicon/dates.py
@@ -1,3 +1,8 @@
+__all__ = [
+    'InvalidDate',
+    'DateWithCalendar'
+]
+
 class InvalidDate(Exception):
     pass
 
--- calexicon/fn/__init__.py
+++ calexicon/fn/__init__.py
@@ -1,2 +1,8 @@
+__all__ = [
+    'iso_to_gregorian',
+    'julian_to_gregorian',
+    'gregorian_to_julian'
+]
+
 from iso import iso_to_gregorian
 from julian import julian_to_gregorian, gregorian_to_julian
