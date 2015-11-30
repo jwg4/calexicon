44f8f63034067dc9a02a3c59332656d631ba741d
Jack <jack.grahl@yahoo.co.uk>
2015-11-30 22:24:01 +0000

    Basic implementation of Julian Day Number.

--- calexicon/calendars/other.py
+++ calexicon/calendars/other.py
@@ -1,5 +1,23 @@
+from datetime import date as vanilla_date
+
 from .base import Calendar
 
 class JulianDayNumber(Calendar):
-    pass
+    first_ce_day = vanilla_date(1, 1, 1)
+    first_ce_day_number = 1000
+    display_name = "Julian Day Number"
+
+    @staticmethod
+    def date_display_string(d):
+        n = JulianDayNumber._day_number(d)
+        return "%d" % n
+
+    @staticmethod
+    def representation(d):
+        return {'day_number': JulianDayNumber._day_number(d)}
+
+    @staticmethod
+    def _day_number(d):
+        return (d - JulianDayNumber.first_ce_day).days + JulianDayNumber.first_ce_day_number
+
 
--- calexicon/calendars/tests/test_other.py
+++ calexicon/calendars/tests/test_other.py
@@ -1,7 +1,16 @@
+from datetime import date as vanilla_date
+
 from calendar_testing import CalendarTest
 
 from calexicon.calendars.other import JulianDayNumber
 
 class TestJulianDayNumber(CalendarTest):
-   calendar_type = JulianDayNumber
+    def setUp(self):
+        self.calendar = JulianDayNumber()
+
+    def test_make_date(self):
+        vd = vanilla_date(2010, 8, 1)
+        d = self.calendar.from_date(vd)
+        self.assertIsNotNone(d)
+
 
