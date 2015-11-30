8c097f07eca52dc37e8d3d4591bb9ee1c05fa310
Jack <jack.grahl@yahoo.co.uk>
2015-11-30 23:11:41 +0000

    Add a new test - on date to day number conversion.

--- calexicon/calendars/tests/test_other.py
+++ calexicon/calendars/tests/test_other.py
@@ -18,3 +18,10 @@ class TestJulianDayNumber(CalendarTest):
         d = self.calendar.from_date(vd)
         self.assertEqual(str(d), 'Day 1721423 (Julian Day Number)')
 
+    def compare_date_and_number(self, year, month, day, number):
+        vd = vanilla_date(year, month, day)
+        d = self.calendar.from_date(vd)
+        self.assertEqual(d.native_representation(), {'day_number': number})
+
+    def test_other_date(self):
+        self.compare_date_and_number(2013, 1, 1, 2456293)
