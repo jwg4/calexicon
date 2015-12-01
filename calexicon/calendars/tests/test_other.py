1d27d4ef7325757a984ba180fa10a837a4c68a93
Jack <jack.grahl@yahoo.co.uk>
2015-12-01 01:27:34 +0000

    Add tests for each first of Jan 2001-2013.

--- calexicon/calendars/tests/test_other.py
+++ calexicon/calendars/tests/test_other.py
@@ -28,5 +28,21 @@ class TestJulianDayNumber(CalendarTest):
         for i in range(25):
             self.compare_date_and_number(1 + 400 * i, 1, 1, 1721423 + days_in_400_years * i)
 
+    def test_first_jan_2000s(self):
+        first_jan_2001 = 2451908
+        self.compare_date_and_number(2001, 1, 1, first_jan_2001)
+        self.compare_date_and_number(2002, 1, 1, first_jan_2001 + 365)
+        self.compare_date_and_number(2003, 1, 1, first_jan_2001 + 365 * 2)
+        self.compare_date_and_number(2004, 1, 1, first_jan_2001 + 365 * 3)
+        self.compare_date_and_number(2005, 1, 1, first_jan_2001 + 365 * 4 + 1)
+        self.compare_date_and_number(2006, 1, 1, first_jan_2001 + 365 * 5 + 1)
+        self.compare_date_and_number(2007, 1, 1, first_jan_2001 + 365 * 6 + 1)
+        self.compare_date_and_number(2008, 1, 1, first_jan_2001 + 365 * 7 + 1)
+        self.compare_date_and_number(2009, 1, 1, first_jan_2001 + 365 * 8 + 2)
+        self.compare_date_and_number(2010, 1, 1, first_jan_2001 + 365 * 9 + 2)
+        self.compare_date_and_number(2011, 1, 1, first_jan_2001 + 365 * 10 + 2)
+        self.compare_date_and_number(2012, 1, 1, first_jan_2001 + 365 * 11 + 2)
+        self.compare_date_and_number(2013, 1, 1, first_jan_2001 + 365 * 12 + 3)
+
     def test_another_date(self):
         self.compare_date_and_number(2013, 1, 1, 2456293)
