9d4d1e56b52084833aa37862c387c76f8e3c3b08
Jack <jack.grahl@yahoo.co.uk>
2015-12-06 22:58:32 +0000

    [flake8] Fix helpers.py.

--- calexicon/helpers.py
+++ calexicon/helpers.py
@@ -1,16 +1,18 @@
 from datetime import date as vanilla_date
 
+
 def ordinal(n):
     suffix = "th"
     if n % 10 == 1:
-        suffix =  "st"
+        suffix = "st"
     if n % 10 == 2:
-        suffix =  "nd"
+        suffix = "nd"
     if n % 10 == 3:
-        suffix =  "rd"
+        suffix = "rd"
     if 10 < n % 100 < 20:
         suffix = "th"
     return "%d%s" % (n, suffix)
+
 
 def month_string(n):
     d = vanilla_date(1995, n, 1)
