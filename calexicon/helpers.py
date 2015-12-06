b7b1ae11378b37350a3fcd9d989be58f655ec986
Jack <jack.grahl@yahoo.co.uk>
2015-12-06 23:35:25 +0000

    Make this part of the function simpler.

--- calexicon/helpers.py
+++ calexicon/helpers.py
@@ -3,12 +3,8 @@ from datetime import date as vanilla_date
 
 def ordinal(n):
     suffix = "th"
-    if n % 10 == 1:
-        suffix = "st"
-    if n % 10 == 2:
-        suffix = "nd"
-    if n % 10 == 3:
-        suffix = "rd"
+    if n % 10 in [1, 2, 3]:
+        suffix = [None, 'st', 'nd', 'rd'][n % 10]
     if 10 < n % 100 < 20:
         suffix = "th"
     return "%d%s" % (n, suffix)
