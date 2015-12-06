e31fd90def33a26a7b9ee5f3c8e1e50ab87ba28e
Jack <jack.grahl@yahoo.co.uk>
2015-12-06 22:39:40 +0000

    [flake8] fix dates/base.py._

--- calexicon/dates/base.py
+++ calexicon/dates/base.py
@@ -1,7 +1,8 @@
-from ..helpers import ordinal, month_string
+
 
 class InvalidDate(Exception):
     pass
+
 
 class DateWithCalendar(object):
     def __init__(self, calendar_class, d):
@@ -18,7 +19,10 @@ class DateWithCalendar(object):
         return self.calendar == other.calendar and self._date == other._date
 
     def __str__(self):
-        return "%s (%s)" % (self.calendar.date_display_string(self._date), self.calendar.display_name)
+        return "%s (%s)" % (
+            self.calendar.date_display_string(self._date),
+            self.calendar.display_name
+        )
 
     def __lt__(self, other):
         try:
@@ -62,12 +66,16 @@ class DateWithCalendar(object):
                 return True
             else:                # pragma: no cover
                 if msg is None:
-                    msg = "%s (%s) is not equal to %s (%s)" % (a, a._date, b, b._date)
+                    msg = (
+                        "%s (%s) is not equal to %s (%s)"
+                        % (a, a._date, b, b._date)
+                    )
                 raise test_case.failureException(msg)
         return assertEqual
 
     def native_representation(self):
         return self.calendar.representation(self._date)
+
 
 class BasicBCEDate(DateWithCalendar):
     def __init__(self, year, month, day):
@@ -81,10 +89,11 @@ class BasicBCEDate(DateWithCalendar):
     @staticmethod
     def _validate(year, month, day):
         if year == 0:
-            raise InvalidDate('There is no year 0 in the Proleptic Julian Calendar')
+            raise InvalidDate(
+                'There is no year 0 in the Proleptic Julian Calendar'
+            )
         lengths = [None, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         if year % 4 == 0:
             lengths[2] = 29
         if day > lengths[month]:
-            raise InvalidDate('That month does not have %d days.' % day) 
-
+            raise InvalidDate('That month does not have %d days.' % day)
