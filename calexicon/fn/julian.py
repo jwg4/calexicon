fcb479447825bed00e449ccc8eeb906bd411d2dd
Jack <jack.grahl@yahoo.co.uk>
2015-12-07 23:00:45 +0000

    Properly implement julian_to_julian_day_number.

--- calexicon/fn/julian.py
+++ calexicon/fn/julian.py
@@ -1,4 +1,5 @@
 from ..calendars import JulianCalendar, ProlepticGregorianCalendar
+from ..calendars import JulianDayNumber, ProlepticJulianCalendar
 
 
 def julian_to_gregorian(year, month, day):
@@ -17,7 +18,10 @@ def gregorian_to_julian(year, month, day):
 
 
 def julian_to_julian_day_number(year, month, day):
-    return 1
+    calendar = ProlepticJulianCalendar()
+    d = calendar.date(year, month, day)
+    converted = d.convert_to(JulianDayNumber())
+    return converted.native_representation()['day_number']
 
 
 def julian_day_number_to_julian(number):
