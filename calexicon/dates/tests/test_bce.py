2050a52b9b3065a9d6211cb029a308dce054c28d
Jack <jack.grahl@yahoo.co.uk>
2015-12-09 22:05:32 +0000

    Add some tests for BCEDate.

---
+++ calexicon/dates/tests/test_bce.py
@@ -0,0 +1,12 @@
+import unittest
+
+from calexicon.dates import BCEDate
+
+class TestBCEDate(unittest.TestCase):
+
+    def test_make_bce_date(self):
+        bd = BCEDate(-4713, 1, 1)
+        self.assertEqual(bd.julian_representation(), (-4713, 1, 1))
+
+    def test_equality(self):
+        self.assertEqual(BCEDate(-44, 3, 15), BCEDate(-44, 3, 15))
