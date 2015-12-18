e24574f88c096d3d5579009f4960a35102f67ca3
Jack <jack.grahl@yahoo.co.uk>
2015-12-18 22:46:29 +0000

    Add a basic test for equality operator.

--- calexicon/tests/test_dates.py
+++ calexicon/tests/test_dates.py
@@ -10,6 +10,9 @@ class TestDateWithCalendar(unittest.TestCase):
         date_dt = date(2010, 8, 1)
         self.date_wc = DateWithCalendar(None, date_dt)
 
+    def test_equality(self):
+        self.assertTrue(self.date_wc != date(2010, 8, 1))
+
     def test_comparisons(self):
         self.assertTrue(self.date_wc < date(2010, 8, 2))
         self.assertFalse(self.date_wc < date(2010, 7, 31))
