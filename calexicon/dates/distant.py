ec8cbe3de5573fadbb54b7d0eae1343af9c281ac
Jack <jack.grahl@yahoo.co.uk>
2015-12-18 23:31:03 +0000

    Add comparison operators for DistantDate.

--- calexicon/dates/distant.py
+++ calexicon/dates/distant.py
@@ -14,3 +14,21 @@ class DistantDate(DateWithCalendar):
 
     def __sub__(self, other):
         return timedelta(days=0)
+
+    def __ge__(self, other):
+        if self.year != other.year:
+            return self.year > other.year
+        if self.month != other.month:
+            return self.month > other.month
+        return self.day >= other.day
+
+    def __le__(self, other):
+        if self.year == other.year and self.month == other.month and self.day == other.day:
+            return True
+        return not (self >= other)
+
+    def __lt__(self, other):
+        return not (self >= other)
+
+    def __gt__(self, other):
+        return not (self <= other)
