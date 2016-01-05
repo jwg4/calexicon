b34e4a78766adc5722f6a91d7545e3cdb8862fd5
Jack <jack.grahl@yahoo.co.uk>
2016-01-05 08:36:45 +0000

    Missing import.

--- calexicon/internal/tests/test_julian.py
+++ calexicon/internal/tests/test_julian.py
@@ -1,5 +1,7 @@
 import unittest
 
+from calexicon.internal.julian import distant_julian_to_gregorian
+
 class TestJulian(unittest.TestCase):
     def test_distant_julian_to_gregorian(self):
         self.assertEqual(distant_julian_to_gregorian(9999, 12, 1), (10000, 2, 12))
