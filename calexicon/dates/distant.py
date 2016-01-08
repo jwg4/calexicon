59cf631ae892e56221982350af606fab80218a64
Jack <jack.grahl@yahoo.co.uk>
2016-01-08 00:11:46 +0000

    Correctly handle the case where the offset_sum would be too big for a vanilla.
    Also, we can equally add a timedelta or an integer number of days.

--- calexicon/dates/distant.py
+++ calexicon/dates/distant.py
@@ -2,6 +2,7 @@ from datetime import timedelta, date as vanilla_date
 
 from base import DateWithCalendar
 from ..internal.julian import distant_julian_to_gregorian
+from ..constants import number_of_days_in_400_gregorian_years
 
 class DistantDate(DateWithCalendar):
     def __init__(self, y, m, d):
@@ -43,9 +44,19 @@ class DistantDate(DateWithCalendar):
         return None
 
     def __add__(self, other):
+        try:
+            n = other.days
+            td = other
+        except:
+            n = other
+            td = timedelta(days=other)
+        if n > number_of_days_in_400_gregorian_years:
+            shifted_date = DistantDate(self.year - 400, self.month, self.day)
+            shift = n - number_of_days_in_400_gregorian_years
+            return shifted_date + shift
         offset = ((self.year - 1600) // 400) * 400
         offset_self = vanilla_date(self.year - offset, self.month, self.day)
-        offset_sum = offset_self + other
+        offset_sum = offset_self + td
         return DistantDate(offset_sum.year + offset, offset_sum.month, offset_sum.day)
 
     def __ge__(self, other):
