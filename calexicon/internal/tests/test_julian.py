9546faddab321eb508f358883faf45cbc7d48dd8
Jack <jack.grahl@yahoo.co.uk>
2016-01-12 23:57:40 +0000

    Correct test - vanilla_date not tuple.

--- calexicon/internal/tests/test_julian.py
+++ calexicon/internal/tests/test_julian.py
@@ -1,5 +1,7 @@
 import unittest
 
+from datetime import date as vanilla_date
+
 from calexicon.internal.julian import distant_julian_to_gregorian, julian_to_gregorian
 
 
@@ -8,4 +10,4 @@ class TestJulian(unittest.TestCase):
         self.assertEqual(distant_julian_to_gregorian(9999, 12, 1), (10000, 2, 12))
 
     def test_julian_to_gregorian(self):
-        self.assertEqual(julian_to_gregorian(1984, 2, 29), (1984, 3, 13))
+        self.assertEqual(julian_to_gregorian(1984, 2, 29), vanilla_date(1984, 3, 13))
