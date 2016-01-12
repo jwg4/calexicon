0788aaf316a2b200c5283fe9f5f902a8da701403
Jack <jack.grahl@yahoo.co.uk>
2016-01-12 23:56:41 +0000

    Add a test for julian_to_gregorian.

--- calexicon/internal/tests/test_julian.py
+++ calexicon/internal/tests/test_julian.py
@@ -1,8 +1,11 @@
 import unittest
 
-from calexicon.internal.julian import distant_julian_to_gregorian
+from calexicon.internal.julian import distant_julian_to_gregorian, julian_to_gregorian
 
 
 class TestJulian(unittest.TestCase):
     def test_distant_julian_to_gregorian(self):
         self.assertEqual(distant_julian_to_gregorian(9999, 12, 1), (10000, 2, 12))
+
+    def test_julian_to_gregorian(self):
+        self.assertEqual(julian_to_gregorian(1984, 2, 29), (1984, 3, 13))
