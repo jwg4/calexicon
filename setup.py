cd51ce66a318826ff0b959b1189ca48c01458051
jwg4 <jwg4@users.noreply.github.com>
2017-07-09 20:08:44 +0000

    Update email address

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Calendar stuff',
     url='http://github.com/jwg4/calexicon',
     author='Jack Grahl',
-    author_email='jack.grahl@yahoo.co.uk',
+    author_email='jack.grahl@gmail.com',
     license='Apache License 2.0',
     packages=['calexicon', 'calexicon.calendars', 'calexicon.internal', 'calexicon.dates', 'calexicon.fn'],
     test_suite='nose.collector',
